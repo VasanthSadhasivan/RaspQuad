a982667ee37f5d2e46455311c08284b40fabc305
Vasanth Sadhasivan <sadh5761@eduhsd.k12.ca.us>
2017-02-09 14:14:55 -0800

    AHHY

--- arduinoQuad/arduinoQuad.ino
+++ arduinoQuad/arduinoQuad.ino
@@ -1,21 +1,6 @@
-#include <Servo.h>
-
-Servo c1,c2,c3,c4,c5,c6;
+#include <TimerOne.h>
 
 void setup() {
-  c1.attach(3);
-  c2.attach(5);
-  c3.attach(6);
-  c4.attach(9);
-  c5.attach(10);
-  c6.attach(11);
-  Serial.begin(9600);
-  c1.write(0);
-  c2.write(0);
-  c3.write(0);
-  c4.write(0);
-  c5.write(0);
-  c6.write(0);
 }
 
 void loop() {
@@ -29,29 +14,29 @@ void translateString(String data){
     data.replace(" ", "");
     String stickName = data.substring(0,data.indexOf(','));
     int power = (data.substring(data.indexOf(',')+1)).toInt();
-    
+    power = power/2;
     if(data.indexOf("c1")!=-1){
-      c1.write(power);
+      Timer1.pwm(3, 51+power, 20000);
       Serial.println(stickName + " " + power);
     }
     else if(data.indexOf("c2")!=-1){
-      c2.write(power);
+      Timer1.pwm(5, 51+power, 20000);
       Serial.println(stickName + " " + power);
     }
     else if(data.indexOf("c3")!=-1){
-      c3.write(power);
+      Timer1.pwm(6, 51+power, 20000);
       Serial.println(stickName + " " + power);
     }
     else if(data.indexOf("c4")!=-1){
-      c4.write(power);
+      Timer1.pwm(9, 51+power, 20000);
       Serial.println(stickName + " " + power);
     }
     else if(data.indexOf("c5")!=-1){
-      c5.write(power);
+      Timer1.pwm(10, 51+power, 20000);
       Serial.println(stickName + " " + power);
     }
     else if(data.indexOf("c6")!=-1){
-      c6.write(power);
+      Timer1.pwm(11, 51+power, 20000);
       Serial.println(stickName + " " + power);
     }
 }
