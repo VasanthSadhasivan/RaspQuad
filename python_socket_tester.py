388ce4352b726bfac1227aadb7e6c13e76c03aef
root <vasanthsadhasivan@gmail.com>
2017-02-17 04:26:39 +0000

    Fixing main file

--- python_socket_tester.py
+++ python_socket_tester.py
@@ -15,11 +15,14 @@ def threadedRead():
                 read_serial=ser.readline()
                 print(read_serial)
 
+def writeData(data, power):
+
+
 while not(internet_on()):
 	pass
 socket_server = socket.socket()
 
-ser = serial.Serial('/dev/ttyAMA0',9600)
+ser = serial.Serial('/dev/ttyAMA0',115200)
 
 socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 try:
@@ -36,12 +39,35 @@ dictionary = {'c1':90,'c2':90,'c3':90,'c4':90,'c5':90,'c6':90}
 
 
 while True:
-	data = remove_padding(str(conn.recv(100).decode('utf-8')).replace('\n',''))
+	data = remove_padding(str(conn.recv(20).decode('utf-8')).replace('\n',''))
 	if 'poweroff' in data:
 		os.system("sudo poweroff")
 	try:
-		dictionary[data.split(',')[0]] = int(data.split(',')[1].replace(' ',''))
-		ser.write(data+'\n')
-		print '[+] Sent Data: '+data
+		power = int(data.split(',')[1].replace(' ',''))
+		if 'c1' in data:
+			print('#1 P'+str((1960*power/180+520))+' T1\n\r')
+			ser.write('#1 P'+str((1960*power/180+520))+' T1\n\r')
+		if 'c2' in data:
+			print('#3 P'+str((1960*power/180+520))+' T1\n\r')
+			ser.write('#3 P'+str((1960*power/180+520))+' T1\n\r')
+                if 'c3' in data:
+                        print('#4 P'+str((1960*power/180+520))+' T1\n\r')
+			ser.write('#4 P'+str((1960*power/180+520))+' T1\n\r')
+                if 'c4' in data:
+                        print('#7 P'+str((1960*power/180+520))+' T1\n\r')
+			ser.write('#7 P'+str((1960*power/180+520))+' T1\n\r')
+                if 'c5' in data:
+			print('#8 P'+str((1960*power/180+520))+' T1\n\r')
+                        ser.write('#8 P'+str((1960*power/180+520))+' T1\n\r')
+                if 'c6' in data:
+			print('#9 P'+str((1960*power/180+520))+' T1\n\r')
+                        ser.write('#9 P'+str((1960*power/180+520))+' T1\n\r')
+		
 	except:
 		pass
+	#try:
+	#	dictionary[data.split(',')[0]] = int(data.split(',')[1].replace(' ',''))
+	#	ser.write(data+'\n')
+	#	print '[+] Sent Data: '+data
+	#except:
+	#	pass
