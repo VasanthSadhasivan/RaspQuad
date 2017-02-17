64593de1e377c1757efc6ca5b20d3411c2594988
Vasanth Sadhasivan <Vasanth Sadhasivan>
2017-02-16 21:00:20 -0800

    Maybe fixed python code

--- python_socket_tester.py
+++ python_socket_tester.py
@@ -11,81 +11,81 @@ def internet_on():
         return False
 
 def remove_padding(padded_text):
-	return padded_text.replace('*','')
+    return padded_text.replace('*','')
 
 def threadedRead():
     while True:
             read_serial=ser.readline()
             print(read_serial)
-			
+            
 def writeData(data, power):
-	if 'c1' in data:
-		print('#1 P'+str((1960*power/180+520))+' T1\n\r')
-		for i in range(dictionary['c1'], power):
-			ser.write('#1 P'+str((1960*i/180+520))+' T1\n\r')
-			time.sleep(.005)
-		ser.write('#1 P'+str((1960*power/180+520))+' T1\n\r')
-		dictionary['c1'] = power
-	elif 'c2' in data:
-		print('#3 P'+str((1960*power/180+520))+' T1\n\r')
-		for i in range(dictionary['c2'], power):
-			ser.write('#3 P'+str((1960*i/180+520))+' T1\n\r')
-			time.sleep(.005)
-		ser.write('#3 P'+str((1960*power/180+520))+' T1\n\r')
-		dictionary['c2'] = power
+    if 'c1' in data:
+        print('#1 P'+str((1960*power/180+520))+' T1\n\r')
+        for i in range(dictionary['c1'], power):
+            ser.write('#1 P'+str((1960*i/180+520))+' T1\n\r')
+            time.sleep(.005)
+        ser.write('#1 P'+str((1960*power/180+520))+' T1\n\r')
+        dictionary['c1'] = power
+    elif 'c2' in data:
+        print('#3 P'+str((1960*power/180+520))+' T1\n\r')
+        for i in range(dictionary['c2'], power):
+            ser.write('#3 P'+str((1960*i/180+520))+' T1\n\r')
+            time.sleep(.005)
+        ser.write('#3 P'+str((1960*power/180+520))+' T1\n\r')
+        dictionary['c2'] = power
     elif 'c3' in data:
-	    print('#4 P'+str((1960*power/180+520))+' T1\n\r')
-		for i in range(dictionary['c3'], power):
-			ser.write('#4 P'+str((1960*i/180+520))+' T1\n\r')
-			time.sleep(.005)
-		ser.write('#4 P'+str((1960*power/180+520))+' T1\n\r')
-		dictionary['c3'] = power
+        print('#4 P'+str((1960*power/180+520))+' T1\n\r')
+        for i in range(dictionary['c3'], power):
+            ser.write('#4 P'+str((1960*i/180+520))+' T1\n\r')
+            time.sleep(.005)
+        ser.write('#4 P'+str((1960*power/180+520))+' T1\n\r')
+        dictionary['c3'] = power
     elif 'c4' in data:
-	    print('#7 P'+str((1960*power/180+520))+' T1\n\r')
-		for i in range(dictionary['c4'], power):
-			ser.write('#7 P'+str((1960*i/180+520))+' T1\n\r')
-			time.sleep(.005)
-		ser.write('#7 P'+str((1960*power/180+520))+' T1\n\r')
-		dictionary['c4'] = power
+        print('#7 P'+str((1960*power/180+520))+' T1\n\r')
+        for i in range(dictionary['c4'], power):
+            ser.write('#7 P'+str((1960*i/180+520))+' T1\n\r')
+            time.sleep(.005)
+        ser.write('#7 P'+str((1960*power/180+520))+' T1\n\r')
+        dictionary['c4'] = power
     elif 'c5' in data:
-		print('#8 P'+str((1960*power/180+520))+' T1\n\r')	
-		for i in range(dictionary['c5'], power):
-			ser.write('#8 P'+str((1960*i/180+520))+' T1\n\r')
-			time.sleep(.005)
+        print('#8 P'+str((1960*power/180+520))+' T1\n\r')    
+        for i in range(dictionary['c5'], power):
+            ser.write('#8 P'+str((1960*i/180+520))+' T1\n\r')
+            time.sleep(.005)
         ser.write('#8 P'+str((1960*power/180+520))+' T1\n\r')
-		dictionary['c5'] = power
+        dictionary['c5'] = power
     elif 'c6' in data:
-		print('#9 P'+str((1960*power/180+520))+' T1\n\r')
-		for i in range(dictionary['c6'], power):
-			ser.write('#9 P'+str((1960*i/180+520))+' T1\n\r')
-			time.sleep(.005)
+        print('#9 P'+str((1960*power/180+520))+' T1\n\r')
+        for i in range(dictionary['c6'], power):
+            ser.write('#9 P'+str((1960*i/180+520))+' T1\n\r')
+            time.sleep(.005)
         ser.write('#9 P'+str((1960*power/180+520))+' T1\n\r')
-		dictionary['c5'] = power
+        dictionary['c5'] = power
 
 while not(internet_on()):
-	pass
+    pass
 socket_server = socket.socket()
 
 ser = serial.Serial('/dev/ttyAMA0',115200)
 
 socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 try:
-	socket_server.bind(('', 90))
+    socket_server.bind(('', 90))
 except socket.error as msg:
-	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-	sys.exit()	
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()    
 socket_server.listen(1)
 conn, addr = socket_server.accept()
 print 'Connected'
 thread.start_new_thread(threadedRead,())
 
 while True:
-	data = remove_padding(str(conn.recv(20).decode('utf-8')).replace('\n',''))
-	if 'poweroff' in data:
-		os.system("sudo poweroff")
-	try:
-		power = int(data.split(',')[1].replace(' ',''))
-		writeData(data, power)
-		
-	except:
-		pass
+    data = remove_padding(str(conn.recv(20).decode('utf-8')).replace('\n',''))
+    if 'poweroff' in data:
+        os.system("sudo poweroff")
+    try:
+        power = int(data.split(',')[1].replace(' ',''))
+        writeData(data, power)
+        
+    except:
+        pass
