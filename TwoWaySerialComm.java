203c95b8dc3cd02ed1c064ef163e480a9e225d37
Vasanth Sadhasivan <vasanthsadhasivan@gmail.com>
2017-02-21 13:17:38 -0800

    Removed unused sub-classes SerialReader and SerialWriter

--- TwoWaySerialComm.java
+++ TwoWaySerialComm.java
@@ -25,13 +25,14 @@ public class TwoWaySerialComm {
 
     void connect ( String portName ) throws Exception {
         CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+		
         if ( portIdentifier.isCurrentlyOwned() ) {
             if(debugging)
                 System.out.println("[-] Error: Port is currently in use");
         }
+		
         else {
             CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
-
             if ( commPort instanceof SerialPort) {
                 SerialPort serialPort = (SerialPort) commPort;
                 serialPort.setSerialPortParams(115200,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
@@ -39,10 +40,8 @@ public class TwoWaySerialComm {
                 InputStream in = serialPort.getInputStream();
                 OutputStream out = serialPort.getOutputStream();
                 outputStream=out;
-                //(new Thread(new SerialReader(in))).start();
-                //(new Thread(new SerialWriter(out))).start();
-
             }
+			
             else {
                 if(debugging)
                     System.out.println("[-] Error: Only serial ports are handled by this example.");
@@ -74,51 +73,6 @@ public class TwoWaySerialComm {
 		        }
                 channels.put("c"+channelNumber, power);
                 return;
-            }
-        }
-    }
-
-    public static class SerialReader implements Runnable {
-        InputStream in;
-
-        public SerialReader ( InputStream in )
-        {
-            this.in = in;
-        }
-
-        public void run () {
-            byte[] buffer = new byte[1024];
-            int len = -1;
-            try {
-                while ( ( len = this.in.read(buffer)) > -1 ){
-                    System.out.print(new String(buffer,0,len));
-                }
-            }
-            catch ( IOException e ) {
-		        System.out.print("[-]");
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static class SerialWriter implements Runnable {
-        OutputStream out;
-
-        public SerialWriter ( OutputStream out )
-        {
-            this.out = out;
-        }
-
-        public void run () {
-            try {
-                int c = 0;
-                while ( ( c = System.in.read()) > -1 ){
-                    this.out.write(c);
-                }
-            }
-            catch ( IOException e ) {
-		System.out.print("[-]");
-                e.printStackTrace();
             }
         }
     }
